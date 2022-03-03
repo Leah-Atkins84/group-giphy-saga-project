481cd8e29c7cc51cabd9570ab4f70285db541c1a
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-03-03 13:58:08 -0600

    created SearchPage component and axios.get saga function on '/search'

--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -1,7 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import axios from 'axios'
+import Button from '@mui/material/Button';
+import { useHistory } from 'react-router-dom';
+import ButtonBack from '../ButtonBack/ButtonBack';
+
 function SearchPage() {
+
+    const dispatch = useDispatch();
+
+    // react useState for feedback from the database to be stored in
+    const [searchedImages, setSearchedImages] = useState([]);
+
     return (
         <>
-            Search Page!
+            <H2>SEARCH FOR SUPA GIFS!</H2>
+
+            <SearchBar />
+
+            {searchedImages.map((image) => {
+                <SearchItems key={image.id} image={image} />
+            })}
+
         </>
     )
 }
